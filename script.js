d2d363b566e5aa66e9c6f6cc3b2b5c5427e3ff00
Sergij Pankiv <pankivsergij97@gmail.com>
2023-10-10 20:17:34 +0300

    Revert "some updates was made"
    This reverts commit e9338db6170792ef3ec8f0a000bfc0fc0cfed9ef.

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 // Variables
 let inputLocation = document.getElementById("location");
 const locateButton = document.getElementById("locate");
-const currentLocation = document.querySelector(
-  ".app__weather__widget__location"
-);
+let currentLocation = document.querySelector(".app__weather__widget__location");
 const currentWeatherDetails = document.querySelector(".app__weather__details");
 const appWeek = document.querySelector(".app__weather__week");
 const celsius = "&#8451";
@@ -18,14 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
   getCoordinates().onload;
 });
 
-// Getting coordinates if geolocation is allowed
-function getCoordinates(position) {
-  getWeatherData(position.coords.latitude, position.coords.longitude);
-  getCity(position.coords.latitude, position.coords.longitude);
-  console.log(position.coords.latitude, position.coords.longitude);
-}
-
-// Getting city name from input
+//Getting city name from input
 let locationPoint = "";
 function handleLocationInput() {
   locationPoint = inputLocation.value.toLocaleString("en");
@@ -121,6 +112,12 @@ if ("geolocation" in navigator) {
 function errorCallback(error) {
   console.log("Error getting location: " + error.message);
   switchVisibility();
+}
+
+//Getting coordinates if geolocation is allowed
+function getCoordinates(position) {
+  getWeatherData(position.coords.latitude, position.coords.longitude);
+  getCity(position.coords.latitude, position.coords.longitude);
 }
 
 //Creating overlay and spinner elements
@@ -297,7 +294,6 @@ function setCurrentDayData(weatherData, city) {
 
   currentLocation = document.querySelector(".app__weather__widget__location");
   currentLocation.textContent = city;
-  console.log(weatherData);
 }
 
 //Changing background according to tht weather
